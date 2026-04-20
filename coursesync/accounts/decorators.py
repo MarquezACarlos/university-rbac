50b15606b4fe7e950b0bd8eb29afb4487027588b
bookmasterj <97921543+bookmasterj@users.noreply.github.com>
2026-04-19 22:26:34 -0500

    Add files via upload

--- coursesync/accounts/decorators.py
+++ coursesync/accounts/decorators.py
@@ -8,8 +8,10 @@ def role_required(*allowed_roles):
         def wrapper(request, *args, **kwargs):
             if not request.user.is_authenticated:
                 raise PermissionDenied
-            if request.user.role not in allowed_roles:
-                raise PermissionDenied
-            return view_func(request, *args, **kwargs)
+            # Allow access if the user has the required role 
+            # OR if they are a sysadmin (to support impersonation)
+            if request.user.role in allowed_roles or request.user.role == 'sysadmin':
+                return view_func(request, *args, **kwargs)
+            raise PermissionDenied
         return wrapper
     return decorator
